3aae2440b50b43fe000ec080fcc6ab1b481a5112
Daniel Ruiz <daniel.ruiz2@atos.net>
2017-11-22 22:52:39 -0600

    BlackList Initial Commit
    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SigninComponent } from "app/auth/signin.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    LoginComponent, SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
